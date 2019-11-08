961acd971a2ef5b5eaec13ef7d134c551a26ccb0
Dyo <lloydmur4@gmail.com>
2019-11-08 15:52:43 -0500

    JSON-server seems to be garbage

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -12,16 +12,15 @@ const todos = [
   { title: 'Eat Corn', completed: false}
 ]
 */
-const url_todos = 'http://localhost:4000/todos/'
+const url_todos = 'http://localhost:4500/todos/'
 const headers = {'content-type': 'application/json'}
 
 class TodoApp extends React.Component {
   constructor(props){
-    super(props);
+    super(props)
     this.state = {
       todos: [],
-      newItem: '',
-      allSelected: false
+      newItem: ''
     }
   }
   //Lifecycle method Runs when this component mounts the DOM
@@ -29,41 +28,67 @@ class TodoApp extends React.Component {
     this.fetchTodos()
   }
 
-  fetchTodos =  () => {
+  fetchTodos = () => {
     fetch(url_todos)
-      .then((data) => data.json()) //Convert data/response to JSON format
-      .then((todos) => this.setState({todos: todos}))
+      .then(function(response){
+        console.log('Converting Data...')
+        return response.json()
+      }) //Convert data/response to JSON format
+      .then((data) =>{
+        console.log('Fetching...')
+        this.setState({todos: data})
+      })
       .catch((err) => console.error({err}))
   }
 
-  masterCheckboxHandler(){
-    let copy = this.state.todos
-    let allS = this.state.allSelected
-    allS = !allS
-    copy.forEach((item) => {
-      item.completed = allS
-    })
-    this.setState({todos: copy, allSelected: allS})
+  toggleAllHandler(allS){
+    const list  = this.state.todos
+    Promise.all(
+      list.map((item, i) => (
+        fetch(url_todos + item.id, {
+          method: 'PATCH',
+          headers,
+          body: JSON.stringify({completed: !allS})
+        }).then(console.log(item))
+      ))
+    ).then(this.fetchTodos)
   }
 
-  checboxClickHandler(item, index){
+  checboxClickHandler(item){
+    /*
     let copy = this.state.todos     //copy the todo item list from app.state.todos
     copy[index].completed = !item.completed     //invert completed
     this.setState( {todos: copy })             //inject copy back into state
+    */
+    const {id, completed} = item
+    fetch(url_todos + id, {
+      method: 'PATCH',
+      headers,
+      body: JSON.stringify({completed: !completed})
+    }).then(this.fetchTodos)
   }
-  deleteHandler(id){
+
+  deleteHandler = (id) => {
     console.log(url_todos + id);
     fetch(url_todos + id, {
       method: 'DELETE',
       headers,
-    })
-      .then(
-        let copy = this.state.todos
-        const filteredList = copy.filter(
-          (target) => target.id != id
-        )
-        this.setState({todos: filteredList})
+    }).then(this.fetchTodos)
+  }
+
+  clearHandler = () => {
+    let copy = this.state.todos
+    let filteredCopy = copy.filter(target => target.completed)
+
+    //Promise.all allows us to return an array of promises fro fetch requests
+    Promise.all(
+      filteredCopy.map(item =>
+        fetch(url_todos + item.id, {
+          method: 'DELETE',
+          headers
+        }).then(this.fetchTodos)
       )
+    )
   }
 
   // event is auto passed to function on event
@@ -105,30 +130,39 @@ class TodoApp extends React.Component {
   }
 
   render(){
-    const {todos} = this.state
+    const { todos } = this.state
+    const allS = todos.every((item) => item.completed)
     return (
       <div className='TodoApp'>
 
         {todos.length === 0 ? (<div>Nothing to see here.</div> ) :
-        <Table>
-          <Table.Header >
-            <Table.Row>
-              <Table.Cell>
-                <Checkbox
-                  id='select-all'
-                  checked= {this.state.allSelected}
-                  onChange= {() => this.masterCheckboxHandler()}
-                />
-              </Table.Cell>
-              <Table.Cell>
-              <label htmlFor="select-all">Select All</label>
-              </Table.Cell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {renderTodoList(this)}
-          </Table.Body>
-        </Table>}
+        <div>
+          <Table>
+            <Table.Header >
+              <Table.Row>
+                <Table.Cell>
+                  <Checkbox
+                    id='select-all'
+                    checked= {allS}
+                    onChange= {() => this.toggleAllHandler(allS)}
+                  />
+                </Table.Cell>
+                <Table.Cell>
+                <label htmlFor="select-all">Select All</label>
+                </Table.Cell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {renderTodoList(this)}
+            </Table.Body>
+          </Table>
+          <button
+            className="ui button"
+            onClick={() => this.clearHandler()}
+          >Clear Completed
+          </button>
+        </div>
+        }
         <div className="mid-col">
           <input
             id='new-item'
@@ -160,7 +194,7 @@ function renderTodoList(app){
       <ListItem
         key={i}
         item={item}
-        handleToggle={() => app.checboxClicHandler(item, i)}
+        handleToggle={() => app.checboxClickHandler(item)}
         handleDelete= {() => app.deleteHandler(item.id)}
       />
     ))
